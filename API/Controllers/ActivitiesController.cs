0640ad58038ccacf444627593c80a87a4dea1746
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-19 16:54:49 -0700

    refactor: Use Mediator for CRUD operations in ActivitiesController

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,21 +7,39 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Activity = Domain.Activity;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
         [HttpGet] //api/activities
-        public async Task<ActionResult<List<Domain.Activity>>> GetActivities()
+        public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //api/activities/id
-        public async Task<ActionResult<Domain.Activity>> GetActivity(Guid id)
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost] //api/activities
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")] //api/activities/id
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Activity = activity });
+
+            return Ok();
         }
     }
 }
