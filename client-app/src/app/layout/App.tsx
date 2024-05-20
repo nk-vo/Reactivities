457cb6d3306496daf79a875fa8d162b52b746815
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-19 22:45:57 -0700

    refactor: Update App component to use typed state for activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react'
 import './styles.css'
 import axios from 'axios'
 import { Header, List } from 'semantic-ui-react'
+import { Activity } from '../models/Activity'
 
 function App() {
-  const [activities, setActivities] = useState([])
+  const [activities, setActivities] = useState<Activity[]>([])
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
         console.log(response)
         setActivities(response.data)
@@ -18,7 +19,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Reactivities'></Header>
       <List>
-        {activities.map((activity: any) => {
+        {activities.map(activity => {
           return (
             <List.Item key={activity.id}>
               {activity.title}
