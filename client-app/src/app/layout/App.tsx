d0fee18065d8d5193dc4a364e1f05ff724757025
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-22 23:22:24 -0700

    chore: Update npm dependencies for uuid package

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,7 @@ import { Container } from 'semantic-ui-react'
 import { Activity } from '../models/Activity'
 import NavBar from './NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
+import { v4 as uuid } from 'uuid'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -39,7 +40,7 @@ function App() {
   function handleCreateOrEditActivity(activity: Activity) {
     activity.id
       ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, activity])
+      : setActivities([...activities, { ...activity, id: uuid() }])
     setEditMode(false)
     setSelectedActivity(activity)
   }
