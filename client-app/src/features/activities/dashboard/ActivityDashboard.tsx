fb0dbedf4510b2784d689d69aeb6a776e00ae652
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-22 18:17:21 -0700

    refactor: Add ActivityDetails and ActivityForm components to ActivityDashboard

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,8 @@
 import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/Activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface Props {
   activities: Activity[];
@@ -11,6 +13,11 @@ export default function ActivityDashboard({activities}: Props) {
     <Grid>
       <Grid.Column width='10'>
         <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width='6'>
+        {activities[0] && 
+        <ActivityDetails activity={activities[0]} />}
+        <ActivityForm />
       </Grid.Column>
     </Grid>
 
