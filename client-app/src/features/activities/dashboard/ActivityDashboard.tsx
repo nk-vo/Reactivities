08695aa41408d52cc65c7082bff35572b3d2180c
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-24 22:18:46 -0700

    refactor: Add CRUD functionality to Activities API

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,9 +14,10 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
   return (
     <Grid>
       <Grid.Column width='10'>
@@ -33,7 +34,12 @@ export default function ActivityDashboard({ activities, selectedActivity, select
             openForm={openForm}
           />}
         {editMode &&
-          <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+          <ActivityForm 
+          closeForm={closeForm} 
+          activity={selectedActivity} 
+          createOrEdit={createOrEdit} 
+          submitting={submitting}
+          />}
       </Grid.Column>
     </Grid>
 
