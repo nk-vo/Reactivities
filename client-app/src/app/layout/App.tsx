defe080d5e1ef6a46e4e470b1a3856101f110b80
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-25 19:55:40 -0700

    refactor: Update NavBar component to use activityStore for openForm function

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -12,7 +12,6 @@ import { observer } from 'mobx-react-lite'
 
 function App() {
   const {activityStore} = useStore();
-
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -22,22 +21,7 @@ function App() {
     activityStore.loadActivities();
   }, [activityStore])
 
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id))
-  }
-
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined)
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity()
-    setEditMode(true)
-  }
-
-  function handleFormClose() {
-    setEditMode(false)
-  }
+  
 
   function handleCreateOrEditActivity(activity: Activity) {
     setSubmitting(true)
@@ -71,16 +55,10 @@ function App() {
 
   return (
     <>
-      <NavBar openForm={handleFormOpen} />
+      <NavBar />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
           activities={activityStore.activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
