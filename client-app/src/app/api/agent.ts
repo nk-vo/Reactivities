6dc722194926e6217c22add18c03b0feb229c5c9
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-25 19:15:22 -0700

    refactor: Update agent.ts to use TypeScript strict mode

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -24,7 +24,9 @@ const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const request = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  // eslint-disable-next-line @typescript-eslint/ban-types
   post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+  // eslint-disable-next-line @typescript-eslint/ban-types
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
   del: <T>(url: string) => axios.delete<T>(url).then(responseBody)
 }
