8529dba90c77362e9ab9ddceaf05cf774bc423c3
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-05-30 12:40:13 -0700

    refactor: Update react-router-dom to version 6.23.1

---
+++ client-app/src/router/Routes.tsx
@@ -0,0 +1,77 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import App from "../app/layout/App";
+import HomePage from "../home/HomePage";
+import ActivityDashboard from "../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../features/activities/form/ActivityForm";
+import ActivityDetails from "../features/activities/details/ActivityDetails";
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/activities",
+        element: <ActivityDashboard />,
+      },
+      {
+        path: "/createActivity",
+        element: <ActivityForm />,
+      },
+      {
+        path: "/activities/:id",
+        element: <ActivityDetails />,
+      },
+      {
+        path: "/errors",
+        element: <div>Errors</div>,
+      },
+      {
+        path: "/server-error",
+        element: <div>Server Error</div>,
+      },
+      {
+        path: "/login",
+        element: <div>Login</div>,
+      },
+      {
+        path: "/profile",
+        element: <div>Profile</div>,
+      },
+      {
+        path: "/register",
+        element: <div>Register</div>,
+      },
+      {
+        path: "/errors",
+        element: <div>Errors</div>,
+      },
+      {
+        path: "/server-error",
+        element: <div>Server Error</div>,
+      },
+      {
+        path: "/login",
+        element: <div>Login</div>,
+      },
+      {
+        path: "/profile",
+        element: <div>Profile</div>,
+      },
+      {
+        path: "/register",
+        element: <div>Register</div>,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+];
+
+export const router = createBrowserRouter({ routes: [] });
