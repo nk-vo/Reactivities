bf8a2c1bfcd2678aa72aec62762c45536c646b16
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-09 10:38:15 -0700

    refactor: Update ActivitiesController and BaseApiController for better error handling and response handling

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -13,5 +13,13 @@ namespace API.Controllers
     {
         private IMediator _mediator; // Change to IMediator
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>(); // Change to Mediator
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null) return NotFound();
+            return BadRequest(result.Error);
+        }
     }
 }
