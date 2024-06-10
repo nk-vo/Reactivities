255f7872cbd675947fc7d7412704fb8a7b7f4182
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-10 16:12:37 -0700

    refactor: Update axios error handling and toast notifications for API requests

---
+++ client-app/src/features/errors/NotFound.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button, Header, Icon, Segment } from "semantic-ui-react";
+
+
+export default function NotFound() {
+  return (
+    <div>
+      <Segment placeholder>
+        <Header icon>
+          <Icon name='search' />
+          Oops - we've looked everywhere and could not find this.
+        </Header>
+      </Segment>
+      <Segment.Inline>
+        <Button as={Link} to='/activities' primary>
+          Return to activities page
+        </Button>
+      </Segment.Inline>
+    </div>
+  )
+}
--- client-app/src/router/Routes.tsx
+++ client-app/src/router/Routes.tsx
@@ -1,10 +1,11 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../app/layout/App";
 import HomePage from "../home/HomePage";
 import ActivityDashboard from "../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../features/activities/form/ActivityForm";
 import ActivityDetails from "../features/activities/details/ActivityDetails";
 import TestErrors from "../features/errors/TestError";
+import NotFound from "../features/errors/NotFound";
 
 export const routes: RouteObject[] = [
   {
@@ -17,6 +18,8 @@ export const routes: RouteObject[] = [
       { path: "/createActivity", element: <ActivityForm />, },
       { path: "/manage/:id", element: <ActivityForm />, },
       { path: "errors", element: <TestErrors />, },
+      { path: "/not-found", element: <NotFound />, },
+      { path: "*", element: <Navigate replace to={'/not-found'} />, },
     ],
   },
   {
