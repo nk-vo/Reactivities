fe46761dcf530583bb428d72badb74a31a98c8b2
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-13 17:57:01 -0700

    feat: Add Identity services to the application

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,36 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Persistence;
+
+namespace API.Extensions
+{
+  public static class IdentityServiceExtensions
+  {
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+    {
+      services.AddIdentityCore<AppUser>(opt =>
+      {
+        opt.Password.RequireNonAlphanumeric = false;
+      })
+          .AddEntityFrameworkStores<DataContext>();
+          // .AddSignInManager<SignInManager<AppUser>>();
+
+      // var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+
+      // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      //     .AddJwtBearer(opt =>
+      //     {
+      //       opt.TokenValidationParameters = new TokenValidationParameters
+      //       {
+      //         ValidateIssuerSigningKey = true,
+      //         IssuerSigningKey = key,
+      //         ValidateIssuer = false,
+      //         ValidateAudience = false
+      //       };
+      //     });
+      services.AddAuthentication();
+
+      return services;
+    }
+  }
+}
