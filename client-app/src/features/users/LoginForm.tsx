0b8b4c9435fc76a8b25035ca4399918d2795661c
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-16 16:39:29 -0700

    refactor: Add UserStore to store.ts and update LoginForm to use mobx-react-lite observer

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,19 +1,22 @@
 import { Form, Formik } from "formik"
 import MyTextInput from "../../app/common/form/MyTextInput"
 import { Button } from "semantic-ui-react"
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../app/stores/store";
 
-export default function LoginForm() {
+export default observer( function LoginForm() {
+  const { userStore } = useStore();
   return (
     <Formik
       initialValues={{ email: '', password: '' }}
-      onSubmit={values => console.log(values)}>
-      {({ handleSubmit }) => (
+      onSubmit={values => userStore.login(values)}>
+      {({ handleSubmit, isSubmitting }) => (
         <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
           <MyTextInput placeholder="Email" name='email' />
           <MyTextInput placeholder="Password" name='password' type='password' />
-          <Button positive content='Login' type='submit' fluid />
+          <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
         </Form>
       )}
     </Formik>
   )
-}
+})
