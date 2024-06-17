4db3ec71cc606816fab2d88d0ff21ac703e9812d
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-16 22:02:41 -0700

    refactor: Update NavBar component to include user authentication and display user avatar and dropdown menu

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,20 +1,39 @@
-import {Button, Container, Menu} from "semantic-ui-react";
+import { Button, Container, Menu, Image, Dropdown } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function NavBar() {
-    return (
-        <Menu inverted fixed='top'>
-            <Container>
-                <Menu.Item as={NavLink} to='/' header>
-                    <img src='/assets/logo.png' alt='logo' style={{marginRight: 10}}/>
-                    Reactivities
-                </Menu.Item>
-                <Menu.Item as={NavLink} to='/activities' name='Activities' />
-                <Menu.Item as={NavLink} to='/errors' name='Errors' />
-                <Menu.Item>
-                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
-                </Menu.Item>
-            </Container>
-        </Menu>
-    )
-}
+export default observer(function NavBar() {
+  const { userStore: { isLoggedIn, user, logout } } = useStore();
+  return (
+    <Menu inverted fixed='top'>
+      <Container>
+        <Menu.Item as={NavLink} to='/' header>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 10 }} />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/activities' name='Activities' />
+        <Menu.Item as={NavLink} to='/errors' name='Errors' />
+        <Menu.Item>
+          <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
+        </Menu.Item>
+        {isLoggedIn ? (
+          <Menu.Item position='right'>
+            <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
+            <Dropdown pointing='top left' text={user?.displayName}>
+              <Dropdown.Menu>
+                <Dropdown.Item as={NavLink} to={`/profile/${user?.username}`} text='My Profile' icon='user' />
+                <Dropdown.Item onClick={logout} text='Logout' icon='power' />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+        ) : (
+          <Menu.Item position='right'>
+            <Button as={NavLink} to='/login' content='Login' basic inverted />
+            <Button as={NavLink} to='/register' content='Register' basic inverted style={{ marginLeft: '0.5em' }} />
+          </Menu.Item>
+        )}
+      </Container>
+    </Menu>
+  )
+})
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Container, Header, Segment, Image } from "semantic-ui-react";
+import { useStore } from '../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
-export default function HomePage() {
-    return (
-        <Segment inverted textAlign='center' vertical className='masthead' >
-            <Container text>
-                <Header as='h1' inverted>
-                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
-                    Reactivities
-                </Header>
-                <Header as='h2' inverted content='Welcome to Reactivities' />
-                <Button as={Link} to='/login' size='huge' inverted>
-                    Login!
-                </Button>
-            </Container>
-        </Segment>
-    )
-}
+export default observer(function HomePage() {
+  const { userStore } = useStore();
+
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead' >
+      <Container text>
+        <Header as='h1' inverted>
+          <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
+          Reactivities
+        </Header>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header as='h2' inverted content='Welcome to Reactivities' />
+            <Button as={Link} to='/activities' size='huge' inverted>
+              Go to activities!
+            </Button>
+          </>
+        ) : (
+          <>
+            <Header as='h2' inverted content='Welcome to Reactivities' />
+            <Button as={Link} to='/login' size='huge' inverted>
+              Login!
+            </Button>
+          </>
+        )}
+        <Header as='h2' inverted content='Welcome to Reactivities' />
+        <Button as={Link} to='/login' size='huge' inverted>
+          Login!
+        </Button>
+      </Container>
+    </Segment>
+  )
+});
