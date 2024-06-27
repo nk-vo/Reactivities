c6c356ece757cf384c317244087c79905a4be93b
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-26 23:28:31 -0700

    refactor: Update MappingProfiles.cs to include lazy loading for user images

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -16,8 +16,9 @@ namespace Application.Core
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
-                // .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos
-                //     .FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos
+                    .FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
