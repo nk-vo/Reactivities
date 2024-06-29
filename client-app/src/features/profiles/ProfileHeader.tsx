aee7014c1db6961a13031af30d1e220607a86952
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-28 23:25:45 -0700

    refactor: Update ProfileHeader component to display profile image and display name

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -1,16 +1,21 @@
-import { Button, Divider, Grid, Item, Reveal, Segment, Statistic } from "semantic-ui-react";
+import { Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
+import { observer } from "mobx-react-lite";
 
-export default function ProfileHeader() {
+interface Props {
+  profile: Profile;
+}
+
+export default observer(function ProfileHeader({ profile }: Props) {
   return (
     <Segment>
       <Grid>
         <Grid.Column width={12}>
           <Item.Group>
             <Item>
-              <Item.Image avatar size='small' circular src={'/assets/user.png'} />
+              <Item.Image avatar size='small' src={profile.image || '/assets/user.png'} />
               <Item.Content verticalAlign="middle">
-                <Item.Header content='Display Name' />
-                <Item.Description>Username</Item.Description>
+                <Header as='h1' content={profile.displayName} />
               </Item.Content>
             </Item>
           </Item.Group>
@@ -38,4 +43,4 @@ export default function ProfileHeader() {
       </Grid>
     </Segment>
   )
-}
+})
