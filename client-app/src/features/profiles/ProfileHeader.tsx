2c18071d8a6bf3ad53bdecabf5cfa14d4396eb35
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-28 22:53:17 -0700

    feat: Add ProfileHeader component to ProfilePage

---
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -0,0 +1,41 @@
+import { Button, Divider, Grid, Item, Reveal, Segment, Statistic } from "semantic-ui-react";
+
+export default function ProfileHeader() {
+  return (
+    <Segment>
+      <Grid>
+        <Grid.Column width={12}>
+          <Item.Group>
+            <Item>
+              <Item.Image avatar size='small' circular src={'/assets/user.png'} />
+              <Item.Content verticalAlign="middle">
+                <Item.Header content='Display Name' />
+                <Item.Description>Username</Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <Statistic.Group widths={2}>
+            <Statistic label='Followers' value='5' />
+            <Statistic label='Following' value='42' />
+          </Statistic.Group>
+          <Divider />
+          <Reveal animated='move'>
+            <Reveal.Content visible style={{ width: '100%' }}>
+              <Button fluid color='teal' content='Following' />
+            </Reveal.Content>
+            <Reveal.Content hidden style={{ width: '100%' }}>
+              <Button
+                fluid
+                basic
+                color={true ? 'red' : 'green'}
+                content={true ? 'Unfollow' : 'Follow'}
+              />
+            </Reveal.Content>
+          </Reveal>
+        </Grid.Column>
+      </Grid>
+    </Segment>
+  )
+}
