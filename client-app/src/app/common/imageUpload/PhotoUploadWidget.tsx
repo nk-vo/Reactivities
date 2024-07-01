3c075c8eddab0e4cf98cf4aca8a1c6502f3ab69a
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-06-30 23:31:43 -0700

    refactor: Add lazy loading for images in PhotoUploadWidget component

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,14 +1,24 @@
-import { Grid, Header } from "semantic-ui-react";
+import { useState } from "react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import PhotoUploadWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState<any[]>([]);
+
   return (
     <Grid>
       <Grid.Column width={4}>
         <Header sub color='teal' content='Step 1 - Add Photo' />
+        <PhotoUploadWidgetDropzone setFiles={setFiles} />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
         <Header sub color='teal' content='Step 2 - Resize image' />
+        {files.length > 0 && (
+          <>
+            <Image src={files[0].preview} />
+          </>
+        )}
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
