2f5c0826fec95b1c525967dd3336e01da226e4dd
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-01 17:28:12 -0700

    refactor: Update PhotoUploadWidget to handle image cropping and uploading

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
-import { Button, Grid, Header, Image } from "semantic-ui-react";
+import { Button, Grid, Header } from "semantic-ui-react";
 import PhotoUploadWidgetDropzone from "./PhotoWidgetDropzone";
 import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
-export default function PhotoUploadWidget() {
+interface Props {
+  uploadPhoto: (file: Blob) => void;
+  loading: boolean;
+}
+
+export default function PhotoUploadWidget({ uploadPhoto, loading }: Props) {
   const [files, setFiles] = useState<any[]>([]);
   const [cropper, setCropper] = useState<Cropper | null>(null);
 
   function onCrop() {
     if (cropper) {
-      cropper.getCroppedCanvas().toBlob((blob: any) => {
-        setFiles([...files, blob]);
-      }, 'image/jpeg');
+      cropper.getCroppedCanvas().toBlob((blob: any) => uploadPhoto(blob!));
     }
   }
 
@@ -41,8 +44,8 @@ export default function PhotoUploadWidget() {
           <>
             <div className="img-preview" style={{ minHeight: 200, overflow: "hidden" }} />
             <Button.Group widths={2}>
-              <Button onClick={onCrop} positive icon='check' />
-              <Button onClick={() => setFiles([])} icon='close' />
+              <Button loading={loading} onClick={onCrop} positive icon='check' />
+              <Button disabled={loading} onClick={() => setFiles([])} icon='close' />
             </Button.Group>
           </>}
       </Grid.Column>
