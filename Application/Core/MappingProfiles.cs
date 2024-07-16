f5295ce6acaa2b351186f7d9a68385bd255ec3f2
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-15 22:41:43 -0700

    feat: Update MappingProfiles to include current user's following status in Profile model

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -20,11 +20,14 @@ namespace Application.Core
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.AppUser.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.AppUser.Followings.Count));
+                .ForMember(d => d.Following, o => o.MapFrom(s => s.AppUser.Followers.Any(x => x.Observer.UserName == currentUsername)));
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d => d.Image, s => s.MapFrom(o => o.Photos.FirstOrDefault(x => x.IsMain).Url))
-                .ForMember(d => d.FollowersCount, s => s.MapFrom(o => o.Followers.Count))
-                .ForMember(d => d.FollowingCount, s => s.MapFrom(o => o.Followings.Count));
-                .ForMember(d => d.Following, s => s.MapFrom(o => o.Followers.Any(x => x.Observer.UserName == currentUsername)));
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
+                .ForMember(d => d.Following, o => o.MapFrom(s => s.Followers.Any(x => x.Observer.UserName == currentUsername)));
             CreateMap<Comment, CommentDto>()
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
