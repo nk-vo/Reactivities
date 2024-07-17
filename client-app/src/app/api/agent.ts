7b02d3e3a559b6d1993882fd33ce66ab7cabba36
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-16 22:23:50 -0700

    refactor: Update Profiles API to include updateFollowing method

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -94,7 +94,8 @@ const Profiles = {
     },
     setMainPhoto: (id: string) => axios.post(`/photos/${id}/setMain`, {}),
     deletePhoto: (id: string) => axios.delete(`/photos/${id}`),
-    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile)
+    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile),
+    updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}), 
 }
 
 const agent = {
