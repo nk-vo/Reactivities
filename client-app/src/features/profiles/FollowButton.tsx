08b7d6e5a01c640506e0829bf5f6be740aa0659a
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-16 23:04:56 -0700

    feat: Add follow/unfollow functionality to FollowButton component

--- client-app/src/features/profiles/FollowButton.tsx
+++ client-app/src/features/profiles/FollowButton.tsx
@@ -13,21 +13,28 @@ export default observer(function FollowButton({ profile }: Props) {
     const { updateFollowing, loading } = profileStore;
     if (userStore.user?.username === profile.username) return null;
 
+    function handleFollow(e: React.SyntheticEvent, username: string) {
+        e.preventDefault();
+        profile.following ? updateFollowing(username, false) : updateFollowing(username, true);
+    }
+
     return (
         <Reveal animated='move'>
             <Reveal.Content visible style={{ width: '100%' }}>
                 <Button
                     fluid
                     color='teal'
-                    content='Following'
+                    content={profile.following ? 'Following' : 'Not following'}
                 />
             </Reveal.Content>
             <Reveal.Content hidden>
                 <Button
                     fluid
                     basic
-                    color={true ? 'red' : 'green'}
-                    content={true ? 'Unfollow' : 'Follow'}
+                    color={profile.following ? 'red' : 'green'}
+                    content={profile.following ? 'Unfollow' : 'Follow'}
+                    loading={loading}
+                    onClick={(e) => handleFollow(e, profile.username)}
                 />
             </Reveal.Content>
         </Reveal>
