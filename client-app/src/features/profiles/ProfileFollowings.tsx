61278933d4b8fc010efb82b3fc63ee59635b88e7
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-18 21:41:25 -0700

    refactor: Update ProfileFollowings component to display the correct list of followers

--- client-app/src/features/profiles/ProfileFollowings.tsx
+++ client-app/src/features/profiles/ProfileFollowings.tsx
@@ -5,13 +5,17 @@ import ProfileCard from "./ProfileCard";
 
 export default observer(function ProfileFollowings() {
     const { profileStore } = useStore();
-    const { profile, followings, loadingFollowings } = profileStore;
+    const { profile, followings, loadingFollowings, activeTab } = profileStore;
 
     return (
         <Tab.Pane loading={loadingFollowings}>
             <Grid>
                 <Grid.Column width={16}>
-                    <Header floated='left' icon='user' content={`People following ${profile?.displayName}`} />
+                    <Header 
+                    floated='left' 
+                    icon='user' 
+                    content={activeTab === 3 ? `People following ${profile?.displayName}` : `People ${profile?.displayName} is following`}
+                    />
                 </Grid.Column>
                 <Grid.Column width={16}>
                     <Card.Group itemsPerRow={4}>
