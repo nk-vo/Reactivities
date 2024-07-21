0a8bbb8d028a9e8ab9238939fa0f62e99b93c464
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-21 16:53:01 -0700

    refactor: Implement infinite scrolling in ActivityDashboard

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,20 +1,21 @@
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
-import { Button, Grid } from "semantic-ui-react";
+import { Grid, Loader } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
 import ActivityFilters from "./ActivityFilters";
 import ActivityList from './ActivityList';
 import { PagingParams } from "../../../app/models/pagination";
+import InfiniteScroll from "react-infinite-scroller";
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
     const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore;
-    const [loadingNext ,setLoadingNext] = useState(false);
+    const [loadingNext, setLoadingNext] = useState(false);
 
     function handdleGetNext() {
         setLoadingNext(true);
-        setPagingParams( new PagingParams(pagination!.currentPage + 1));
+        setPagingParams(new PagingParams(pagination!.currentPage + 1));
         loadActivities().then(() => setLoadingNext(false));
     }
 
@@ -27,18 +28,20 @@ export default observer(function ActivityDashboard() {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList />
-                <Button 
-                    floated='right'
-                    content='More...'
-                    positive
-                    onClick={handdleGetNext}
-                    loading={loadingNext}
-                    disabled={pagination?.totalPages === pagination?.currentPage}
-                />
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={handdleGetNext}
+                    hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                    initialLoad={false}
+                >
+                    <ActivityList />
+                </InfiniteScroll>
             </Grid.Column>
             <Grid.Column width='6'>
                 <ActivityFilters />
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Loader active={loadingNext} />
             </Grid.Column>
         </Grid>
     )
