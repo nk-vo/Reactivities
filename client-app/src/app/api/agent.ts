f9d6e52438f2ff3381fd6afb093f75210f75409f
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-21 23:36:18 -0700

    refactor: Update agent.ts to include listActivities method in Profiles API

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { Activity, ActivityFormValues } from '../models/activity';
-import { Photo, Profile } from '../models/profile';
+import { Photo, Profile, UserActivity } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { router } from '../router/Routes';
 import { store } from '../stores/store';
@@ -103,7 +103,8 @@ const Profiles = {
     updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile),
     updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
     listFollowings: (username: string, predicate: string) => requests
-        .get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+        .get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+    listActivities: (username: string, predicate: string) => requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 const agent = {
