369216376dee6628cde03040e425fcc6b0fb0c3a
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-23 22:24:03 -0700

    update to React 18 and use Vite instead of Create React App

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,7 +4,6 @@ import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import "react-datepicker/dist/react-datepicker.css";
 import './app/layout/styles.css';
-import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
@@ -21,4 +20,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
