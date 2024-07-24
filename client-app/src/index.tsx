87b7632ef9c9305867989acac0f2934195230ff9
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2024-07-23 22:45:41 -0700

    feat: Add StrictMode to improve component rendering reliability

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -7,14 +7,17 @@ import './app/layout/styles.css';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
+import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StoreContext.Provider value={store}>
-    <RouterProvider router={router} />
-  </StoreContext.Provider>
+  <StrictMode>
+    <StoreContext.Provider value={store}>
+      <RouterProvider router={router} />
+    </StoreContext.Provider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
