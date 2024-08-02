b018df33cc1b4a2248b36deb8eef7e881444d26a
Nick <vo.nick2868@gmail.com>
2024-08-01 23:40:55 -0700

    refactor: Update TargetFramework to net8.0 in csproj files

--- Persistence/Migrations/DataContextModelSnapshot.cs
+++ Persistence/Migrations/DataContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.20")
+                .HasAnnotation("ProductVersion", "8.0.20")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
